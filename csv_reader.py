69139e51ab953ff96f8fa3a36e1abe66e66b7c70
Marek Galvanek <marek.galvanek@gmail.com>
2018-07-18 14:42:10 +0200

    add KU csv parser

---
+++ csv_reader.py
@@ -0,0 +1,18 @@
+import csv
+
+def get_ku_codes(file):
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+                continue
+
+            yield row["KOD"]
+            line_count += 1
+
+
+codes = get_ku_codes('UI_KATASTRALNI_UZEMI.csv')
+for code in codes:
+    print(code)
