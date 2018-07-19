fc7d809d6ee82f3b06f202883a4169e14f3c0e68
Ian Tate <jan.henek@gmail.com>
2018-07-19 23:01:39 +0200

    Parsování URL z první scrapingu.

--- Spiders/SeznamNemovitosti.py
+++ Spiders/SeznamNemovitosti.py
@@ -3,7 +3,6 @@ from scrapy.crawler import CrawlerProcess
 from urllib.parse import urljoin
 from Spiders.items import NextURL
 import json
-import pickle
 
 
 class SeznamNemovitosti(scrapy.Spider):
@@ -15,17 +14,29 @@ class SeznamNemovitosti(scrapy.Spider):
         main_url = "http://nahlizenidokn.cuzk.cz/"
         next_url = NextURL()
 
-        for link in response.css("[summary=Pozemky]"):  # "table.zarovnat" = vsechny tabulky, prvni tabulka = "[summary=Pozemky]"
-            next_url['url'] = link.css("a::attr(href)").extract() #link: ZobrazObjekt.aspx?encrypted=*shitloadkodu==* ktery se musi spojit s predponou "http://nahlizenidokn.cuzk.cz/"
+        for link in response.css(
+                "[summary=Pozemky]"):  # "table.zarovnat" = vsechny tabulky, prvni tabulka = "[summary=Pozemky]"
+            next_url['url'] = link.css(
+                "a::attr(href)").extract()  # link: ZobrazObjekt.aspx?encrypted=*shitloadkodu==* ktery se musi spojit s predponou "http://nahlizenidokn.cuzk.cz/"
             with open('urls.txt', 'w') as file:
-                file.write(json.dumps(next_url.__dict__)) #delete me, I am here just for debug
+                file.write(json.dumps(next_url.__dict__))  # delete me, I am here just for debug
             yield next_url
 
-        next_page = response.css('table.zarovnat a::attr(href)').extract_first()
-        if next_page is not None:
+        # next_page = response.css('table.zarovnat a::attr(href)').extract_first()
+        if next_url is not None:
             next_page = urljoin(main_url, next_page)
             yield response.follow(next_page,
                                   callback=self.parse)  # volam ten stejny parser jako na seznam nemovitosti, coz neni dobre, musim udelat dalsi parser
+
+    def parse_me_url(self, link_dict=NextURL()):
+        if len(link_dict) > 0:
+            urls_as_string = link_dict["url"] #z dictionary si vytáhnu value pro key "url", která je stringem s url
+            list_of_urls = urls_as_string.split(",") #ze stringu parsujeme URL do listu
+            return list_of_urls
+
+    def crawl_those_urls(self):
+        r=0
+        return r
 
 
 process = CrawlerProcess({
