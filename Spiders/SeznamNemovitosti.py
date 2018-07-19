4279ea28b790f089e0251cb95da841fd54ce50e9
Ian Tate <jan.henek@gmail.com>
2018-07-19 19:39:12 +0200

    Win32 error update

--- Spiders/SeznamNemovitosti.py
+++ Spiders/SeznamNemovitosti.py
@@ -1,35 +1,38 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from urllib.parse import urljoin
+from Spiders.items import NextURL
 import json
 
 
-class seznamNemovitosti(scrapy.Spider):
+class SeznamNemovitosti(scrapy.Spider):
     name = "SeznamNemovitosti"
-    start_urls = [
-        "http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=YM1I8cKFwRqtSN1pzaZSv-EXJsX4ZkgaroYrwUMxx0MfYRoGVqejf3vfazStQpZruJm5uurEo63eIDzBPMntEajfuuJxS92jvfJtM4YC-kCfScnV_9Xmbg=="]
+    start_urls = ["http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=6wCf7KhxP-YP6NrJmpLefYQXT9tsI0-CKVvDaw0RHZC1f0IuEeNFMLr4619wYYNBs9tgHz5DeVtqvIMkJbqVUryvGUq7yZUe4Eud9NSqL96bhVIjoDrSkw=="]
 
     def parse(self, response):
         main_url = "http://nahlizenidokn.cuzk.cz/"
+        next_url = NextURL()
 
-        for link in response.css("[summary=Pozemky]"):  # dalsi moznost k "table.zarovnat" je ziskat primo prvni tabulku a to: "[summary=Pozemky]"
-                next_url = link.css("a::attr(href)").extract()
+        for link in response.css(
+                "[summary=Pozemky]"):  # dalsi moznost k "table.zarovnat" je ziskat primo prvni tabulku a to: "[summary=Pozemky]"
+            next_url['url'] = link.css("a::attr(href)").extract()
             # kazdy link ma tvar: ZobrazObjekt.aspx?encrypted=*shitloadkodu==* ktery se musi spojit s predponou "http://nahlizenidokn.cuzk.cz/"
-                yield next_url
+            yield next_url
 
-
-        filename = "seznam nemovitosti.txt"
-        with open(filename, 'wb') as outfile:
-            json.dump(response.body, outfile)
+        #filename = "seznam nemovitosti.txt"
+        #with open(filename, 'wb') as outfile:
+        #   json.dump(response.body, outfile)
 
         next_page = response.css('table.zarovnat a::attr(href)').extract_first()
         if next_page is not None:
             next_page = urljoin(main_url, next_page)
-            yield response.follow(next_page, callback=self.parse) #volam ten stejny parser jako na seznam nemovitosti, coz neni dobre, musim udelat dalsi parser
+            yield response.follow(next_page,
+                                  callback=self.parse)  # volam ten stejny parser jako na seznam nemovitosti, coz neni dobre, musim udelat dalsi parser
+
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
 })
 
-process.crawl(seznamNemovitosti)
+process.crawl(SeznamNemovitosti)
 process.start()
---
+++ Spiders/items.py
@@ -0,0 +1,5 @@
+import scrapy
+
+
+class NextURL(scrapy.Item):
+    url = scrapy.Field()
