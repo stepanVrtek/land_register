fbf4c7503232700edf857c81b5b8d31186aaa41f
Ian Tate <jan.henek@gmail.com>
2018-07-20 18:22:04 +0200

    facelift2 (komenty, uspořádání kódu)

--- Spiders/SeznamNemovitosti.py
+++ Spiders/SeznamNemovitosti.py
@@ -8,41 +8,45 @@ from scrapy.crawler import CrawlerProcess
 
 class SeznamNemovitosti(scrapy.Spider):
     name = "SeznamNemovitosti"
+    # sem pujde vysledek z lr_list_spider misto natvrdo zadane adresy
     start_urls = [
-        "http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=bUrsf-lRpqts8K2Hs6RMGr7HX0ngePvoeENqwacpHvYeDAaFHPMjHGC-ywuB0VXlshMm06eThAMsf0fpS2dnMs-sJgVzO4lUJDDAy5lEnHupuz_LVpSKaw=="]
+        "http://nahlizenidokn.cuzk.cz/ZobrazObjekt.aspx?encrypted=bUrsf"
+        "-lRpqts8K2Hs6RMGr7HX0ngePvoeENqwacpHvYeDAaFHPMjHGC-ywuB0VXlshMm06eThAMsf0fpS2dnMs"
+        "-sJgVzO4lUJDDAy5lEnHupuz_LVpSKaw=="]
 
     def parse(self, response):
         next_url = UrlItem()
 
-        #pokus o parsovani obsahu vrchni tabulky s nemovitostmi, nefunguje :(
+        # pokus o parsovani obsahu vrchni tabulky s nemovitostmi, nefunguje :(
         nemovitosti = Nemovitosti()
         nemovitosti['lv'] = response.xpath('//*[@id="content"]/table[1]/tbody/tr[1]/td[2]/strong/text()').extract()
-        #nemovitosti['ku'] = response.css('a::attr(href) a::text').extract_first()
+        # nemovitosti['ku'] = response.css('a::attr(href) a::text').extract_first()
         print(nemovitosti)
 
-        #získávám odkazy na parcely, které chceme scrapovat
+        # získávám odkazy na parcely, které chceme scrapovat
         for link in response.css(
                 "[summary=Pozemky]"):  # "table.zarovnat" = vsechny tabulky, prvni tabulka = "[summary=Pozemky]"
             next_url['url'] = link.css(
-                "a::attr(href)").extract()  # link: ZobrazObjekt.aspx?encrypted=*shitloadkodu==* ktery se musi spojit s predponou "http://nahlizenidokn.cuzk.cz/"
+                "a::attr(href)").extract()  # link: ZobrazObjekt.aspx?encrypted=*shitloadkodu==* ktery se musi spojit
+            # s predponou "http://nahlizenidokn.cuzk.cz/"
             yield next_url
 
-        #tímto loopem projedu každou další stránku a spustím nad ní další parser: scrap_this_page
+        # tímto loopem projedu každou další stránku a spustím nad ní další parser: scrap_this_page
         list_of_urls = self.parse_me_url(next_url)
         for x in list_of_urls:
             self.scrap_this_page(x)
 
-    #metoda pro převod dict/ itemu s value plne URL na list s jednotlivymi odkazy kompletovanymi s hlavni URL
+    # metoda pro převod dict/ itemu s value plne URL na list s jednotlivymi odkazy kompletovanymi s hlavni URL
     def parse_me_url(self, link_dict=UrlItem()):
         main_url = "http://nahlizenidokn.cuzk.cz/"
         complete_url = []
         if len(link_dict) > 0:
-            urls = link_dict["url"] #z dictionary si vytáhnu value pro key "url", která je stringem s url
+            urls = link_dict["url"]  # z dictionary si vytáhnu value pro key "url", která je stringem s url
             for x in urls:
                 complete_url.append(urljoin(main_url, x))  # spojuji hlavni cast odkazu s vyparsovanou casti odkazu
         return complete_url
 
-    #Scraper pro další odkazy
+    # Scraper pro další odkazy
     def scrap_this_page(self, url):
         r = scrapy.http.Request(url)
         return r
