6b07522b87f2c1dff1a36a32f581cbaae868aa8a
Marek Galvánek <marek.galvanek@gmail.com>
2018-07-24 21:56:17 +0200

    save csv exports to separated files

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ if __name__ == '__main__':
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
 
-    process.crawl(TitleDeedSpider, ku_code = '600016', lv_code = '2')
+    count = 0
+
+    ku_codes = csv_reader.get_ku_codes('land_register/UI_KATASTRALNI_UZEMI.csv')
+    for ku_code in ku_codes:
+        count += 1
+        for i in range(1, 50):
+            process.crawl(TitleDeedSpider, ku_code = ku_code, lv_code = str(i))
+        if count == 10:
+            break
+
     process.start()
-
-    # ku_codes = csv_reader.get_ku_codes('land_register/UI_KATASTRALNI_UZEMI.csv')
-    # for ku_code in ku_codes:
-    #     for i in range(2):
-    #         process = CrawlerProcess({
-    #             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    #         })
-
-    #         process.crawl(TitleDeedSpider, ku_code = ku_code, lv_code = i)
-    #         process.start()
