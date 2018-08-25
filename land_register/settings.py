70c2669c965441f52eb2effba050c47fc3d5dc09
Marek Galvánek <marek.galvanek@gmail.com>
2018-08-25 08:42:52 +0200

    testing storm proxies

--- land_register/settings.py
+++ land_register/settings.py
@@ -21,12 +21,12 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 100
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -75,7 +75,7 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
@@ -94,12 +94,12 @@ ITEM_PIPELINES = {
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-# ROTATING_PROXY_LIST = [
-#     'http://103.245.11.164:53281',
-#     # ...
-# ]
+ROTATING_PROXY_LIST = [
+    'http://95.211.175.167:13401',
+    'http://95.211.175.225:13401'
+]
 
-ROTATING_PROXY_LIST_PATH = 'land_register/proxies_list.txt'
+# ROTATING_PROXY_LIST_PATH = 'land_register/proxies_list.txt'
 
 # Export
 FEED_FORMAT = 'csv'
