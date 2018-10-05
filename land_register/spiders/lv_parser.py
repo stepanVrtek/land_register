13ef8f55ca1c71eca5becdc5835df9466a4af942
Marek Galvánek <marek.galvanek@gmail.com>
2018-10-05 22:46:15 +0200

    common methods - template

---
+++ land_register/spiders/lv_parser.py
@@ -0,0 +1,16 @@
+"""General parse methods for KN LV items."""
+
+def parse_ground(response):
+    pass
+
+def parse_building_object(response):
+    pass
+
+def parse_building(response):
+    pass
+
+def parse_unit(response):
+    pass
+
+def parse_owners(response):
+    pass
