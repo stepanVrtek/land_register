2417a6890676220a68b84d16b7c8b5abe71aa640
Marek Galvánek <marek.galvanek@gmail.com>
2019-02-03 16:36:26 +0100

    add function for forgetten finished jobs

---
+++ land_register/crawlers/status_update.py
@@ -0,0 +1,27 @@
+from land_register import db_handler
+from land_register.crawlers import utils
+from land_register.crawlers.land_register_crawler import (
+    get_scraping_id, get_ku_jobs
+)
+from datetime import datetime
+
+
+def update_finished_statuses():
+    running_py_jobs = get_ku_jobs(get_scraping_id(), 'R')
+
+    scrapy_jobs = utils.get_scrapyd().list_jobs('LandRegisterSpider')
+    running_scrapy_jobs = [j['id'] for j in scrapy_jobs['running']]
+
+    for p in running_py_jobs:
+        if p['hash_ulohy'] in running_py_jobs:
+            continue
+
+
+def update_job_status(job_id, status):
+    db = db_handler.get_dataset()
+    fields = dict(id=job_id, stav=status, datum_konce=datetime.now())
+    db['log_uloha'].update(fields, ['id'])
+
+
+if __name__ == '__main__':
+    update_finished_statuses()
